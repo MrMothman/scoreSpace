b3bd285ec013bc8d8778d11fd1936060cac9ea0c
Porter Runyon-Trapp <runyontrp@sou.edu>
2024-06-21 14:58:00 -0700

    Start of Main Menu currently only exit works needs to map to starting the game or a test for start button needs options button

---
+++ gameJam/scenes/MainMenu/Main_Menu.gd
@@ -0,0 +1,15 @@
+class_name  MainMenu
+extends Control
+
+@onready var StartButton = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
+@onready var ExitButton = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
+@export var StartLevel = preload("res://scenes/levels/level.tscn")
+
+func _ready():
+	StartButton.button_down.connect(on_Start_press)
+	ExitButton.button_down.connect(on_Exit_pressed)
+	
+func on_Start_press()->void:
+	pass
+func on_Exit_pressed() ->void:
+	get_tree().quit()
