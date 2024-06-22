b1a195e5a6b303c0144ce2187fc556e8b2c54ec1
MythicalMongoose <m.mythicalmongoose@gmail.com>
2024-06-22 17:24:53 -0400

    improvements again

--- gameJam/scripts/Camera2D.gd
+++ gameJam/scripts/Camera2D.gd
@@ -3,8 +3,9 @@ extends Camera2D
 # set camera size and stuff here
 
 func _process(delta):
-	print(position)
 	if Input.is_action_pressed("ui_right"):
+		if position.x < 1600:
 			position.x += 1000*delta
 	if Input.is_action_pressed("ui_left"):
+		if position.x > -400:
 			position.x -= 1000*delta
