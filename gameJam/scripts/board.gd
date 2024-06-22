cf04177f77ad9348d6d31231eee03d8be2375349
Mrmothman <morganraltman@gmail.com>
2024-06-22 00:52:44 -0700

    Added card coalCard

--- gameJam/scripts/board.gd
+++ gameJam/scripts/board.gd
@@ -3,18 +3,29 @@ extends Control
 @onready var ap = $AnimationPlayer
 
 var openfinnished: bool = false
+@onready var spawn = $Panel/spawn
 
-func _on_panel_mouse_entered():
-	openfinnished = false
-	ap.play("mouse_on")
-
-func _on_panel_mouse_exited():
-	if openfinnished:
-		ap.play("mouse_off")
-
+@onready var coal_card : PackedScene = preload("res://scenes/objects/cards/coalCard.tscn")
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "mouse_on":
 		openfinnished = true
 		
 		
+
+
+func _on_button_pressed():
+	var card = coal_card.instantiate()
+	spawn.add_child(card)
+
+
+func _on_area_2d_mouse_entered():
+	openfinnished = false
+	ap.play("mouse_on")
+
+
+func _on_area_2d_mouse_exited():
+	if openfinnished:
+		ap.play("mouse_off")
+
+
