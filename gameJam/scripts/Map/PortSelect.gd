31171d0f589a94f14758abe8f9acaa5454e3d533
MythicalMongoose <m.mythicalmongoose@gmail.com>
2024-06-23 00:29:05 -0400

    port map setup

--- gameJam/scripts/Map/PortSelect.gd
+++ gameJam/scripts/Map/PortSelect.gd
@@ -1,11 +1,13 @@
-extends Control
+extends MapGraph
 
 func _ready():
-	$PortSelect.get_popup().add_item("home")
+	$PortSelect.get_popup().add_item(map.keys()[1].port_name)
 	
 	$PortSelect.get_popup().id_pressed.connect(_on_item_pressed)
 
 
 func _on_item_pressed(id):
 	var port_name = $PortSelect.get_popup().get_item_text(id)
+	print(port_name)
 	# func set_dest(port_name)
+
